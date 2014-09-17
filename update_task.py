7ec6f447cf36a4a7ae47d51198c80d7c189e4510
Tomas Ken <tomkenmag@gmail.com>
2014-09-17 15:17:35 +0000

    fix

--- update_task.py
+++ update_task.py
@@ -6,19 +6,17 @@ import re
 if __name__=="__main__":
     tw = TaskWarrior()
     assignment_list = get_assignment_list()
+    tasks = tw.load_tasks()['pending']
     for asg in assignment_list:
         if asg.handin:
             continue
         courseid = asg.courseid[6:]
         taskdescr = '{}: {}'.format(courseid, asg.name)
-        searchptrn = re.sub(r"(?<=\s)(\d+)(?=\s|$)", r"-\1", taskdescr)
-        task = tw.get_task(description=searchptrn)
-        task = task[1]
-        if not task:
+        if not any([taskdescr in t.values() for t in tasks]):
             tw.task_add( taskdescr,
                         {'project' : 'homework:'+courseid,
                          'due' : asg.due_datetime })
-        elif task['status'] == 'pending' and asg.handin:
+            print("taskw: Adding task \"{}\"".format(taskdescr))
+        elif asg.handin:
+            print("taskw: Moving task \"{}\" completed.".format(taskdescr))
             tw.task_done(task)
-
-
